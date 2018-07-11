b91a97fbdb979665388908269c0211c463f03b98
NiCo Velazco <nfvelazco@gmail.com>
2018-07-11 11:36:55 -0300

    Se agrago manejo de selects a los ejemplos

---
+++ EjemplosKnockout/Controllers/SelectsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EjemplosKnockout.Controllers
+{
+    public class SelectsController : Controller
+    {
+        // GET: Selects
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EjemplosKnockout/Scripts/ClientApp/ViewModel/SelectsModel.js
@@ -0,0 +1,32 @@
+﻿
+
+
+
+
+function Model() {
+    var self = this;
+
+    self.FactorySelect = new ListasFactory();
+
+    self.lFilms = ko.observableArray([]);
+
+    self.selectedFilm = ko.observable();
+
+
+    self.Load = function () {
+        self.lFilms([]);
+        self.FactorySelect.getFilms().done(function (result) {
+            var response = new imdbResponse(result);
+            self.lFilms(response.lFilms());
+        });
+    }
+
+    self.Load();
+
+}
+
+$(function () {
+
+    ko.applyBindings(new Model());
+
+});
